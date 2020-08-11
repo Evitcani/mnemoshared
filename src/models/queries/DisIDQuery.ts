75998cc9259c490e95da2bb36629f336e7ae6382
Evitcani <33763283+evitcani@users.noreply.github.com>
2020-08-10 20:17:34 -0500

    Update the discord ID query.

--- src/models/queries/DisIDQuery.ts
+++ src/models/queries/DisIDQuery.ts
@@ -1,8 +1,9 @@
 import {AbstractQuery, ALL_ABSTRACT_QUERY} from "./base/AbstractQuery";
 
 export interface DiscordIDQuery extends AbstractQuery {
+    character_ids?: string | string[];
     character_id?: string;
     world_id?: string;
 }
 
-export const ALL_DISCORD_ID_QUERY: string[] = ALL_ABSTRACT_QUERY.GET().concat(["world_id", "character_id"]);
+export const ALL_DISCORD_ID_QUERY: string[] = ALL_ABSTRACT_QUERY.GET().concat(["world_id", "character_id", "character_ids"]);
